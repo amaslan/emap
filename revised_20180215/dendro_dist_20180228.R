ce0b3d3a10af17040259de89e5170de1beb229ac
Annie Maslan <amaslan@berkeley.edu>
2018-02-28 16:52:06 -0800

    more mut dist work

--- revised_20180215/dendro_dist_20180228.R
+++ revised_20180215/dendro_dist_20180228.R
@@ -17,10 +17,6 @@ library(reshape2)
 corr_of_corr = "/Users/annie/emap/revised_20180215/GO_slims_corr_of_corr.RData"
 output_path = "/Users/annie/emap/revised_20180215/20180228/"
 
-
-# mutants for which we have data
-mut_data <- c("GSP1-NAT", "T34E", "R108L", "H141V", "Q147E")
-
 # PPI partners - added NUP60
 ppi = c("MOG1", "KAP104", "MTR10", "MSN5", "LOS1", "CRM1", "PSE1", "KAP95", "SRP1", "YRB1", "YRB2", "SRM1", "RNA1", "NTF2", "CSE1", "NUP60")
 
@@ -49,24 +45,76 @@ write.table(sep_no_na, file=paste(output_path, method, "_sep_no_na.csv"), quote=
 
 # filter to only include PPI partners and mutants for which we have data
 ppi_only <- filter(sep_no_na, protein %in% ppi)
-mut_ppi_only <- filter(ppi_only, mutant %in% mut_data)
 
 # remove grouped clusters
-final <- filter(ppi_only, grepl("_GO_", cluster))
+# final <- filter(ppi_only, grepl("_GO_", cluster))
+final <- sep_no_na
 
+# just look at strong muts
+#strong_muts1 <- c("D79A", "D79S", "H141E", "H141R", "K101R", "R108L", "R108Q", "R108Y",
+                  #"R112A", "R112S", "R78K", "T34E", "T34G", "T34Q") 
+#strong_muts1 <- c("R108L", "R108Q", "R108Y",
+                 # "T34E", "T34G", "T34Q") 
 
-# protein vs. mutant - 1 per cluster
+strong_muts1 <- c("D79A", "D79S", "H141E", "H141R",
+                  "R112A", "R112S") 
+final <- filter(final, mutant %in% strong_muts1)
+
+# partner vs. mutant - 1 per cluster
 i=1
-#d <- data.frame(matrix(ncol = length(unique(final$cluster)), nrow = 1711))
+m <- c()
 for (c in unique(final$cluster)) {
     s <- final[which(final$cluster == c),]
-    s <- s[,c('mutant', 'protein', 'value')]
-    mat <- spread(s, protein, value)
+    s <- s[,c('mutant', 'partner', 'value')]
+    mat <- spread(s, partner, value)
     rownames(mat) <- mat[,c(1)]
     mat <- mat[,-1]
     mat <- data.matrix(mat)
     d <- dist(mat)
-    m <- data.frame(t(combn(rownames(mat),2)), as.numeric(d), id)
-    names(m) <- c("p1", "p2", "distance", "cluster")
+    tmp <- data.frame(t(combn(rownames(mat),2)), as.numeric(d), c)
+    m <- rbind(m, tmp)
     i = i+1
 }
+
+names(m) <- c("p1", "p2", "distance", "cluster")
+
+m$pair <- paste(m$p1, "-", m$p2)
+
+mat <- spread(m, cluster, distance)
+rownames(mat) <- mat[,c(3)]
+mat <- mat[,!names(mat) %in% c('p1', 'p2', 'pair')]
+mat <- data.matrix(mat)
+d <- dist(mat)
+
+hc <- hclust(d)
+plot(hc)
+
+fit <- cmdscale(d, eig=TRUE, k=2)
+fit
+
+x <- fit$points[,1]
+y <- fit$points[,2]
+plot(x, y, xlab="coordinate 1", ylab='coordinate 2', main='MDS', type='n')
+text(x, y, labels = row.names(mat), cex=.5)
+
+
+f <- data.frame(t(combn(rownames(mat),2)), as.numeric(d))
+names(f) <- c("pair1", "pair2", "distance")
+
+
+ggplot(f, aes(x=pair1, y=distance)) +
+  geom_boxplot(colour = "#1F3552", fill = "#4271AE",
+               size = 1) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
+
+dev.off()
+hmcol <- colorRampPalette(brewer.pal(9, "RdYlBu"))(100)
+gplots::heatmap.2(as.matrix(d), 
+                  trace="none",
+                  col=hmcol,
+                  #margins=c(1,1),
+                  key.title=NA,
+                  main=id)
